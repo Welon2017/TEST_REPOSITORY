fd23eb25bbb0a2eb044972c5af9320eeb3b57a21
Welon <fwl@ldrotbot.com>
2021-07-01 20:20:26 +0800

    给main函数增加相应变化-----1

--- test_program.cpp
+++ test_program.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+int func_main_1()
+{
+    std::cout << "This is a fun 1 for main" << std::endl;
+    return 0;
+}
+
 int main()
 {
-
+    std::cout << "This is main branch" << std::endl;
     return 0;
 }
